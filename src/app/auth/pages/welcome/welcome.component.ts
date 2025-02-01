73929d025fc80e345745115b1ddee4d6f2c6e832
Jorge Chafloque Millones <37223352+jchafloquem@users.noreply.github.com>
2025-02-01 10:50:23 -0500

    welcome final

--- src/app/auth/pages/welcome/welcome.component.ts
+++ src/app/auth/pages/welcome/welcome.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.scss'
 })
